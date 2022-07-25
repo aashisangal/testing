92c1661ee21788ac1744ee2368317b9e738ec49e
Vishwa Gupta <aryan@Vishwas-MBP.attlocal.net>
2022-07-25 17:53:19 -0400

    assignment2 quest2 added

---
+++ A2Q4.java
@@ -0,0 +1,48 @@
+package assignment1;
+import java.util.Scanner;
+class Account {
+	double Balance = 12342;
+}
+class SavingsAccount extends Account {
+	
+	
+}
+
+class CurrentAccount extends Account {
+	float ExtraCharge;
+	double ExtraCharges(double withdrawAmt1) {
+		return ExtraCharge = (float) (withdrawAmt1 * (0.1));
+	}
+}
+
+class Entry extends CurrentAccount {
+	void WithdrawAmount(String account1, double withdrawAmt) {
+		CurrentAccount c1 = new CurrentAccount();
+		c1.ExtraCharges(withdrawAmt);
+		if (account1.equalsIgnoreCase("CurrentAccount")) {
+		
+			Balance = Balance - withdrawAmt - c1.ExtraCharge;
+			System.out.println("Extra charge added as withdrawn from Current Account: $" + c1.ExtraCharge);
+		}
+		else if (account1.equalsIgnoreCase("SavingAccount")) {
+			Balance = Balance - withdrawAmt;
+			System.out.println("No Extra charge ");
+		}
+
+		System.out.println("Balance: $" + Balance);
+	}
+	void MakePayment() {
+	}
+}
+public class A2Q4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter SavingAccount or CurrentAccount");
+		String account = sc.next();
+		System.out.println("Enter withdraw Amount");
+		Double withdrawAmount = sc.nextDouble();
+		Entry e1 = new Entry();
+		e1.WithdrawAmount(account, withdrawAmount);
+		sc.close();
+	}
+}
